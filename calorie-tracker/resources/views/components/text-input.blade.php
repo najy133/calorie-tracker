b4d21fb5faef0b40ba2a34c60ed264204079cdc1
Nagy ElSokkary <nagyelsokkary@Nagys-MacBook-Air.local>
2026-05-16 17:43:14 +0300

    text input fix

--- calorie-tracker/resources/views/components/text-input.blade.php
+++ calorie-tracker/resources/views/components/text-input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input @disabled($disabled) {{ $attributes->merge(['class' => 'border-zinc-300 dark:border-zinc-700 dark:bg-zinc-800 dark:text-zinc-100 dark:placeholder-zinc-500 focus:border-emerald-500 dark:focus:border-emerald-500 focus:ring-emerald-500 dark:focus:ring-emerald-500 rounded-lg shadow-sm transition']) }}>
+<input @disabled($disabled) {{ $attributes->merge(['class' => 'px-3 py-2 border-zinc-300 dark:border-zinc-700 dark:bg-zinc-800 dark:text-zinc-100 dark:placeholder-zinc-500 focus:border-emerald-500 dark:focus:border-emerald-500 focus:ring-emerald-500 dark:focus:ring-emerald-500 rounded-lg shadow-sm transition']) }}>
